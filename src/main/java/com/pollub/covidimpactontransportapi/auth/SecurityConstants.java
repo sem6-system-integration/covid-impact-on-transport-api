c47f7d76b48d949eab47c04b0e9aae5ca5601588
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-12 13:46:54 +0200

    fix: check if db is already seeded before seeding (#10)

--- src/main/java/com/pollub/covidimpactontransportapi/auth/SecurityConstants.java
+++ src/main/java/com/pollub/covidimpactontransportapi/auth/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.pollub.covidimpactontransportapi.auth;
 
 public class SecurityConstants {
     public static final String SECRET = "mBxhqjgPMmffkZfzOGIAFbXbrulJfPaO";
-    public static final Long EXPIRATION_TIME = 900_000L; // 15 min
+    public static final Long EXPIRATION_TIME = 604_800_000L; // 7 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/users";
